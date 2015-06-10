45f183c998b627421f7cc719d4cd604f0453576f
novizarhs <fizarwind@gmail.com>
2015-06-10 16:24:08 +0700

    dari ulang

--- app/Model/event.php
+++ app/Model/event.php
@@ -2,17 +2,5 @@
 	class Event extends AppModel {
 	public $name = 'Event';
 	public $hasMany = array('Member');
-	public $validate = array(
-		'name'=>array(
-			'nama_tidak_boleh_kosong'=>array(
-				'rule'=>'notEmpty',
-				'message'=>'Post ini tidak memiliki nama!'
-			),
-			'nama_tidak_sama'=>array(
-				'rule'=>'isUnique',
-				'message'=>'Post lain dengan nama/judul sama sudah ada!'
-			)
-		)
-	)
 	}
 ?>
