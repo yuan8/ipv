7c5cfbc0177c86cba6b0255b1d5593b6d63b5c04
novizarhs <fizarwind@gmail.com>
2015-06-10 16:08:25 +0700

    hapus file join.php & status_user.php di model

--- app/Model/join.php
+++
@@ -1,6 +0,0 @@
-<?php
-	class Join extends AppModel {
-	public $name = 'Join';
-	public $belongsTo = array('User','Event');
-	}
-?>
--- app/Model/status_user.php
+++
@@ -1,8 +0,0 @@
-<?php
-	class Status_user extends AppModel {
-	public $name = 'Status_user';
-	public $hasMany = array('User');
-	public $hasAndBelongsToMany= array('Event');
-
-	}
-?>
